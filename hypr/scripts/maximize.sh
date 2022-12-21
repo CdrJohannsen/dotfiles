b64f27225bbe8a8bd94ffd1d7a4a8ba302569e13
CdrJohannsen <eisbaer.j.m@gmail.com>
2022-12-21 11:35:58 +0100

    updated dotfiles

--- hypr/scripts/maximize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-gaps=$(hyprctl getoption general:gaps_out -j | jq '.int')
-if [ $gaps == 20 ]; then
-    hyprctl --batch "keyword general:border_size 0 ; keyword general:gaps_out 0 ; keyword general:gaps_in 0 ; keyword decoration:rounding 0"
-else
-    hyprctl --batch "keyword general:border_size 2 ; keyword general:gaps_out 20 ; keyword general:gaps_in 5 ; keyword decoration:rounding 10"
-fi
